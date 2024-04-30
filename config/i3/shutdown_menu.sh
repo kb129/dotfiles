684e9ed17ea51846f84d9056d096c8dc675619bc
kb129 <okod129@gmail.com>
2024-05-01 01:20:51 +0900

    Add suspend behavior

--- config/i3/shutdown_menu.sh
+++ config/i3/shutdown_menu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo -e "logout\nshutdown\nreboot" | dmenu -i -p "Power:" | {
+echo -e "sleep\nlogout\nshutdown\nreboot" | dmenu -i -p "Power:" | {
     read action
     case $action in
+        sleep) systemctl suspend;;
         logout) i3-msg exit;;
         shutdown) systemctl poweroff;;
         reboot) systemctl reboot;;
