56bad1e22e078434146593c69c5ab9110e0f3c95
kb129 <okod129@gmail.com>
2025-11-05 04:19:41 +0900

    Update

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,4 +1,23 @@
 local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+local grp = augroup("TabsToSpacesOnWrite", { clear = true })
 
+autocmd("BufWritePre", {
+    group = grp,
+    pattern = "*",
+    callback = function(args)
+        local bufnr = args.buf
+        if vim.bo[bufnr].filetype == "make" then return end
+        if vim.bo[bufnr].buftype ~= "" then return end
+        if vim.fn.search("\\t", "nw") == 0 then return end
+        local prev_expandtab = vim.bo[bufnr].expandtab
+        local prev_tabstop   = vim.bo[bufnr].tabstop
 
+        vim.bo[bufnr].expandtab = true
+        vim.bo[bufnr].tabstop   = 4
+        vim.cmd("silent keepjumps keepmarks %retab")
+
+        vim.bo[bufnr].expandtab = prev_expandtab
+        vim.bo[bufnr].tabstop   = prev_tabstop
+    end,
+})
