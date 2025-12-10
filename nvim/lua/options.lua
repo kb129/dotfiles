155ba106de11ee97115c4f8aceb4cd6833722ce6
d-okonogi <okonogi.daiki@artic.iir.titech.ac.jp>
2025-12-10 12:27:44 +0900

    leader300ms define ga.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -8,50 +8,50 @@ vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 vim.opt.fileformats = { "unix", "dos", "mac" }
 
-vim.opt.number = true                -- 行番号を表示
-vim.opt.relativenumber = true        -- 相対行番号
-vim.opt.cursorline = true            -- カーソル行をハイライト
-vim.opt.showmatch = true             -- 括弧の対応を表示
-vim.opt.termguicolors = true         -- True Color 有効化
-vim.opt.signcolumn = "yes"           -- 常にサイン列を表示（gitsignsなど用）
+vim.opt.number = true -- 行番号を表示
+vim.opt.relativenumber = true -- 相対行番号
+vim.opt.cursorline = true -- カーソル行をハイライト
+vim.opt.showmatch = true -- 括弧の対応を表示
+vim.opt.termguicolors = true -- True Color 有効化
+vim.opt.signcolumn = "yes" -- 常にサイン列を表示（gitsignsなど用）
 
-vim.opt.wrap = false                 -- 行の折り返しを無効
-vim.opt.scrolloff = 8                -- 上下に余白を確保
+vim.opt.wrap = false -- 行の折り返しを無効
+vim.opt.scrolloff = 8 -- 上下に余白を確保
 vim.opt.sidescrolloff = 8
 
-vim.opt.tabstop = 4                  -- タブ幅
-vim.opt.shiftwidth = 4               -- 自動インデント時のスペース数
-vim.opt.expandtab = true             -- タブをスペースに変換
-vim.opt.smartindent = true           -- スマートインデント
+vim.opt.tabstop = 4 -- タブ幅
+vim.opt.shiftwidth = 4 -- 自動インデント時のスペース数
+vim.opt.expandtab = true -- タブをスペースに変換
+vim.opt.smartindent = true -- スマートインデント
 
-vim.opt.ignorecase = true            -- 検索時に大文字小文字を無視
-vim.opt.smartcase = true             -- ただし大文字を含むときは区別
-vim.opt.incsearch = true             -- 検索入力中にハイライト
-vim.opt.hlsearch = true              -- 検索結果をハイライト
+vim.opt.ignorecase = true -- 検索時に大文字小文字を無視
+vim.opt.smartcase = true -- ただし大文字を含むときは区別
+vim.opt.incsearch = true -- 検索入力中にハイライト
+vim.opt.hlsearch = true -- 検索結果をハイライト
 
-vim.opt.clipboard = "unnamedplus"    -- システムクリップボード共有
-vim.opt.mouse = "a"                  -- マウス操作を有効化
+vim.opt.clipboard = "unnamedplus" -- システムクリップボード共有
+vim.opt.mouse = "a" -- マウス操作を有効化
 
-vim.opt.updatetime = 250             -- CursorHold イベントの遅延を短縮
-vim.opt.timeoutlen = 400             -- マッピングの待ち時間（短め）
+vim.opt.updatetime = 250 -- CursorHold イベントの遅延を短縮
+vim.opt.timeoutlen = 300 -- マッピングの待ち時間（短め）
 
-vim.opt.splitbelow = true            -- 横分割は下に開く
-vim.opt.splitright = true            -- 縦分割は右に開く
+vim.opt.splitbelow = true -- 横分割は下に開く
+vim.opt.splitright = true -- 縦分割は右に開く
 
-vim.opt.swapfile = false             -- スワップファイルを作らない
+vim.opt.swapfile = false -- スワップファイルを作らない
 vim.opt.backup = false
 vim.opt.writebackup = false
-vim.opt.undofile = true              -- 永続 undo
+vim.opt.undofile = true -- 永続 undo
 
 -----------------------------------------------------------
 -- 表示関連
 -----------------------------------------------------------
-vim.opt.laststatus = 3               -- グローバルステータスライン
-vim.opt.showmode = false             -- モード表示を消す（lualineが代替）
-vim.opt.cmdheight = 1                -- コマンドラインの高さ
-vim.opt.pumheight = 10               -- 補完ポップアップの最大行数
-vim.opt.conceallevel = 2             -- Markdownなどでのconceal動作
-vim.opt.list = true                  -- 不可視文字を表示
+vim.opt.laststatus = 3 -- グローバルステータスライン
+vim.opt.showmode = false -- モード表示を消す（lualineが代替）
+vim.opt.cmdheight = 1 -- コマンドラインの高さ
+vim.opt.pumheight = 10 -- 補完ポップアップの最大行数
+vim.opt.conceallevel = 2 -- Markdownなどでのconceal動作
+vim.opt.list = true -- 不可視文字を表示
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -----------------------------------------------------------
@@ -71,9 +71,9 @@ vim.g.maplocalleader = ","
 -----------------------------------------------------------
 -- 余白に表示する記号など
 vim.opt.fillchars = {
-  eob = " ",         -- バッファ末尾の~を非表示
-  fold = " ",        -- 折りたたみ行のマーク
-  diff = "╱",        -- diff表示のマーク
+    eob = " ", -- バッファ末尾の~を非表示
+    fold = " ", -- 折りたたみ行のマーク
+    diff = "╱", -- diff表示のマーク
 }
 
 -----------------------------------------------------------
